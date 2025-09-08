3b8e4ece8ab779294eec277fc613aaebb8a90d3b
Safee715 <princesafee0@gmail.com>
2025-09-08 15:14:42 +0500

    Ui fixes

---
+++ lib/pages/onboardingScreens/viewmodel/onboarding_page_viewmodel.dart
@@ -0,0 +1,41 @@
+import 'package:DummyInvoice/data/helpers/constants.dart';
+import 'package:DummyInvoice/pages/onboardingScreens/onboarding_page1.dart';
+import 'package:DummyInvoice/pages/onboardingScreens/onboarding_page2.dart';
+import 'package:flutter/cupertino.dart';
+
+class OnboardingPageViewmodel extends ChangeNotifier{
+int currentPage=0;
+List<Widget> onBoardingContent=
+    [OnboardingPage1(),OnboardingPage2()];
+final PageController pageController=PageController();
+int getCurrentPage()
+{
+  return currentPage;
+}
+void setCurrentPage(int value)
+{
+   currentPage=value;
+   notifyListeners();
+
+}
+void nextPage()
+{
+  if(currentPage==0)
+    {
+pageController.nextPage(duration: Duration(milliseconds: 300), curve:Curves.easeInOut);
+    }
+}
+void previous()
+{
+  if(currentPage==1)
+  {
+    pageController.previousPage(duration: Duration(milliseconds: 300), curve:Curves.easeInOut);
+  }
+
+}
+
+
+
+
+
+}
