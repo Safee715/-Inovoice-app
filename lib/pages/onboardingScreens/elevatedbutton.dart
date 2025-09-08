3af8a0409374549c206861cbb72874bdde7c693c
Safee715 <princesafee0@gmail.com>
2025-09-08 15:15:41 +0500

    Ui fixes

---
+++ lib/pages/onboardingScreens/elevatedbutton.dart
@@ -0,0 +1,89 @@
+import 'package:DummyInvoice/data/helpers/constants.dart';
+import 'package:DummyInvoice/data/helpers/extensions.dart';
+import 'package:DummyInvoice/pages/home_page/view/home_page.dart';
+import 'package:DummyInvoice/pages/navigation_pages/view/main_navigation_page.dart';
+import 'package:DummyInvoice/pages/onboardingScreens/viewmodel/onboarding_page_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NavigationButton extends StatefulWidget {
+  const NavigationButton({super.key,required this.text});
+final String text;
+  @override
+  State<NavigationButton> createState() => _NavigationButtonState();
+}
+
+class _NavigationButtonState extends State<NavigationButton> {
+  Constants constants=Constants();
+  @override
+  Widget build(BuildContext context) {
+    final onboardingPageViewmodel=context.watch<OnboardingPageViewmodel>();
+    return
+      Container(
+        width: context.getWidth(360),
+        decoration: BoxDecoration(
+          borderRadius:
+          BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff9CD9FF),
+              Color(0xff4082E3),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: ElevatedButton(
+          onPressed: () {
+            if(onboardingPageViewmodel.getCurrentPage()==0)
+              {
+                onboardingPageViewmodel.pageController.nextPage(
+                    duration: Duration(
+                    milliseconds: 300),
+                    curve: Curves.easeInOut);
+
+              }
+            else {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return MainNavigationPage(constant: constants);
+                  },
+                ),
+              );
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            padding:
+            EdgeInsets.symmetric(
+              vertical: context.getAdaptiveSizeLength(10),
+            ),
+            overlayColor:
+            Colors.transparent,
+            shadowColor:
+            Colors.transparent,
+            backgroundColor:
+            Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius:
+              BorderRadiusGeometry.circular(
+                  context.getWidth(10)
+              ),
+            ),
+          ),
+          child: Text(
+            widget.text,
+            style: TextStyle(
+              color: Color(0xffffffff),
+              fontFamily: 'Satoshi',
+              fontSize: context.getAdaptiveSizeLength(22),
+              letterSpacing: 1,
+              fontWeight:
+              FontWeight.bold,
+            ),
+          ),
+        ),
+      );
+  }
+}
---
+++ lib/pages/onboardingScreens/onboarding_page.dart
@@ -0,0 +1,64 @@
+import 'package:DummyInvoice/data/helpers/constants.dart';
+import 'package:DummyInvoice/data/helpers/extensions.dart';
+import 'package:DummyInvoice/pages/onboardingScreens/elevatedbutton.dart';
+import 'package:DummyInvoice/pages/onboardingScreens/viewmodel/onboarding_page_viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() =>
+      _OnboardingPageState();
+}
+
+class _OnboardingPageState
+    extends State<OnboardingPage> {
+  Constants constants=Constants();
+
+  @override
+  Widget build(BuildContext context) {
+    final onboardingPageViewmodel=context.watch<OnboardingPageViewmodel>();
+    Constants constants = Constants();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Theme.of(context).getContainerColor(),
+      body: Container(
+
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(
+          bottom: 20,
+          top: 20,
+        ),
+        decoration: BoxDecoration(
+          gradient: Theme.of(
+            context,
+          ).getOnboardingScreenBackGradient(),
+        ),
+        child: Column(
+          mainAxisAlignment:
+          MainAxisAlignment.center,
+          children: [
+           Expanded(
+             child: PageView.builder(
+               onPageChanged: (value) => onboardingPageViewmodel.setCurrentPage(value),
+               controller: onboardingPageViewmodel.pageController,
+               itemCount: 2,
+               itemBuilder: (context, index) {
+                return Container(
+                   child: onboardingPageViewmodel.onBoardingContent[index],
+                );
+             },
+             ),
+           ),
+      NavigationButton(
+        text: onboardingPageViewmodel.getCurrentPage()==0
+            ?constants.Next
+            :constants.GetStarted)
+          ],
+        ),
+      ),
+    );
+  }
+}
